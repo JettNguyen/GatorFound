9f49005caf245c016a4b0112284b432689d3b087
thanizmy <thanhtran@ufl.edu>
2024-10-14 15:00:57 -0400

    Add user authentication backend

---
+++ server/backend/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        min: 4,
+        max: 20,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 50,
+    },
+    password: {
+        type: String,
+        require: true,
+        min: 6,
+    },
+}, {
+    timestamps: true
+});  
+
+const User = mongoose.model('User', UserSchema);
+export default User;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from 'express';
+
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import {connectDB} from "./backend/config/db.js";
+import userRoutes from "./backend/routes/user.route.js";
 
 dotenv.config();
 
@@ -10,18 +13,17 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/GatorFound', userRoutes);
+// // Database connection
+// mongoose.connect(process.env.MONGO_URI)
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch((err) => console.error('Failed to connect to MongoDB:', err));
 
-// Database connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('Failed to connect to MongoDB:', err));
 
-// Sample route
-app.get('/', (req, res) => {
-    res.send('Hello from the backend!');
-});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server running on port ${PORT}`);
 });
